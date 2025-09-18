9d76274b87d8ba257320f17da66c00374c437089
eduardomeloalbanese <eduardoalbaneze99@gmail.com>
2025-09-18 16:00:34 -0300

    fix: consolida erros que estava dando no App.tsx com Header.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,11 @@ import { IntegrantesPage } from './pages/Integrantes';
 
 const Home = () => <h2 className="text-yellow-500">Página Inicial</h2>;
 const Lembretes = () => <h2>Página de Lembretes</h2>;
-const Recompensas = () => <h2 className=''>Página de Recompensa</h2>;
+const Diario = () => <h2>Página do Diário da Saúde</h2>;
+const Cuidador = () => <h2>Página do Cuidador</h2>;
 const Contato = () => <h2>Página de Contato</h2>;
+const Sobre = () => <h2>Página Sobre</h2>;
 const FAQ = () => <h2>Página de FAQ</h2>;
- 
-
-
-
 function App() {
   return (
     <>
@@ -21,9 +19,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/lembretes" element={<Lembretes />} />
-        <Route path="/recompensas" element={<Recompensas />} />
+        <Route path="/diario-da-saude" element={<Diario />} />
+        <Route path="/cuidador" element={<Cuidador />} />
         <Route path="/contato" element={<Contato />} />
         <Route path="/integrantes" element={<IntegrantesPage />} />
+        <Route path="/sobre" element={<Sobre />} />
         <Route path="/faq" element={<FAQ />} />
       </Routes>
       </main>
