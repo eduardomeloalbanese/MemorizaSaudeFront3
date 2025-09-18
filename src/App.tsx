3e336582293aa21da720ae9bf310d2503845ab19
Guilherme de Andrade Martini <guilherme.martini2807@gamil.com>
2025-09-18 14:50:52 -0300

    feat: implementa a pagina de integrantes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import { IntegrantesPage } from './pages/Integrantes';
 
 const Home = () => <h2 className="text-yellow-500">Página Inicial</h2>;
 const Lembretes = () => <h2>Página de Lembretes</h2>;
 const Recompensas = () => <h2 className=''>Página de Recompensa</h2>;
 const Contato = () => <h2>Página de Contato</h2>;
-const Integrantes = () => <h2>Página de Integrantes</h2>;
 const FAQ = () => <h2>Página de FAQ</h2>;
  
 
@@ -23,7 +23,7 @@ function App() {
         <Route path="/lembretes" element={<Lembretes />} />
         <Route path="/recompensas" element={<Recompensas />} />
         <Route path="/contato" element={<Contato />} />
-        <Route path="/integrantes" element={<Integrantes />} />
+        <Route path="/integrantes" element={<IntegrantesPage />} />
         <Route path="/faq" element={<FAQ />} />
       </Routes>
       </main>
---
+++ src/pages/Integrantes.tsx
@@ -0,0 +1,33 @@
+import { MemberCard } from '../components/MemberCard';
+
+export function IntegrantesPage() {
+  return (
+    <div className="flex flex-col items-center p-8">
+      <h1 className="text-3xl font-bold mb-8">Identificação dos Alunos</h1>
+
+      <div className="flex flex-wrap justify-center">
+        <MemberCard
+          name="Nathan Gonçalves Pereira Mendes"
+          rm="564666"
+          photoUrl="src/assets/foto_nathan.jpg"
+          linkedinUrl="https://www.linkedin.com/in/nathangpmendes"
+          githubUrl="http://github.com/nathangpm"
+        />
+        <MemberCard
+          name="Guilherme de Andrade Martini"
+          rm="566087"
+          photoUrl="src/assets/foto_guilherme.jpg"
+          linkedinUrl="https://www.linkedin.com/in/guilherme-de-andrade-martini-994648348"
+          githubUrl="https://github.com/Guilhermedev2807"
+        />
+        <MemberCard
+          name="Eduardo de Melo Albanese"
+          rm="561790"
+          photoUrl="src/assets/foto_eduardo.png"
+          linkedinUrl="https://www.linkedin.com/in/eduardo-de-melo-albanese-605043236/"
+          githubUrl="https://github.com/eduardomeloalbanese"
+        />
+      </div>
+    </div>
+  );
+}
