d81f29ca6b33c761ecc9046d391f898f804235b6
eduardomeloalbanese <eduardoalbaneze99@gmail.com>
2025-11-07 16:49:34 -0300

    Fix: Correção de cor padrão de botão para #7faf95

--- src/pages/LembretesPage.tsx
+++ src/pages/LembretesPage.tsx
@@ -262,7 +262,7 @@ export function LembretesPage() {
                     </div>
                     <button
                         type="submit"
-                        className="w-full bg-blue-600 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-700 transition-colors"
+                        className="w-full bg-[#7faf95] text-white font-bold py-2 px-4 rounded-md hover:bg-opacity-80 transition-colors"
                     >
                         Salvar Lembrete
                     </button>
